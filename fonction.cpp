cb2d2dca66069093f07452dbd347da26e199fe4e
Nekoth93 <alexandre.deletraz@hotmail.com>
2021-11-12 17:42:18 +0100

    fonctionPlus

--- fonction.cpp
+++ fonction.cpp
@@ -4,7 +4,7 @@
 
   Auteur(s)   : Alexandre Delétraz et Tomas Pavoni
   Date        : 12.11.2021
-  But         :
+  But         : Avoir une librairie de fonction.
 
   Remarque(s) : à compléter
 
@@ -16,23 +16,22 @@
 #include <cstdlib>
 #include <string>
 #include <limits>
+#include <ctime>
+#include "fonction.h"
 
-unsigned long int getInt() {
+long int getInt(const long MIN, const long MAX) {
 
-   using namespace std;
       // ------------------------------------------
-      // saisie une valeur entre MIN et MAX
+      //    saisie une valeur entre MIN et MAX
       // ------------------------------------------
-      const               string MSG_ERROR = "/!\\ erreur de saisie ..."s;
-      const unsigned long MIN = 0,
-                          MAX = ULONG_MAX;
-      unsigned  long      value;
-      bool                error;
+      long value;
+      bool error;
 
       do {
          // message et saisie
-         cout << "saisie [" << MIN << " - " << MAX << "] : ";
-         cin >> value;
+         cout << "Veuillez entrer une valeur entre [" << MIN << " et "
+              << MAX << "] : ";
+         cin  >> value;
 
          // saisie et vérification en même temps
          // erreur = not(cin >> saisie) or saisie < MIN or saisie > MAX;
@@ -51,4 +50,70 @@ unsigned long int getInt() {
       return value;
    }
 
+char charGenerator(int rand1_26){
+   char c = char('a' + rand1_26);
 
+   return c;
+
+}
+
+int intGenerator1_26(){
+
+   static bool first = true;
+   if (first) {
+      srand((unsigned)time(NULL));
+      first = false;
+   }
+   int rand1_26 = rand() % 26;              // valeur [1-26]
+
+   return rand1_26;
+}
+
+char getC(const char MIN_CHAR, const char MAX_CHAR){
+
+   // --------------------------------------------------
+   // saisie une valeur entre MIN_CHAR et MAX_CHAR
+   // --------------------------------------------------
+
+   char userChar = 0;
+   bool error;
+
+   do {
+      // message et saisie
+      cout << "Saisisez le caractère" << endl;
+
+      // vérification
+      error = cin.fail() or userChar < MIN_CHAR or userChar > MAX_CHAR;
+      if (error) {
+         cout << MSG_ERROR << endl;
+         cin.clear();
+      }
+      // vider buffer
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+   } while(error);
+
+   return userChar;
+}
+
+bool tryAgain(){
+   char userChar;
+   bool error;
+
+   do {
+      // message et saisie
+      cin >> userChar;
+
+      // vérification
+      error = cin.fail() or userChar !='n' or userChar != 'o';
+      if (error) {
+         cout << MSG_ERROR << endl;
+         cin.clear();
+      }
+      // vider buffer
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+   } while(error);
+
+   return userChar;
+}
--- fonction.h
+++ fonction.h
@@ -3,28 +3,60 @@
 //
 #include <cstdlib>
 #include <iostream>
+#include <string>
 
 #ifndef LABO05_CHRONO_FONCTION_H
 #define LABO05_CHRONO_FONCTION_H
 
-unsigned long int getInt() {
+using namespace std;
+const           string MSG_ERROR = "/!\\ erreur de saisie ..."s;
 
-}
+//***********************************************************************************
+// nom         getInt
+// but         fonction de saisie d'un entier (long int) non signé pour contrôlé
+//             la saisie.
+// param       Reçoit en paramètre les valeur MAX et MIN.
+// return      valeur saisie dans la fonction
+// exception   n/a
+//***********************************************************************************
+long int getInt(const long MIN, const long MAX);
 
-char charGenerator(int rand1_26) {
+//***********************************************************************************
+// nom         charGenerator
+// but         Fonction qui génère un caractère aléatoire.
+// param       Une valeur numérique générée aléatoirement par la fonction
+//             intGenerator.
+// return      Un caractère
+// exception   n/a
+//***********************************************************************************
+char charGenerator(int rand1_26);
 
-}
+//***********************************************************************************
+// nom         intGenerator
+// but         Fonction qui génère un entier aléatoire.
+// param       Aucun
+// return      Un entier
+// exception   n/a
+//***********************************************************************************
+int intGenerator1_26();
 
-int intGenerator1_26() {
+//***********************************************************************************
+// nom         getC
+// but         Fonction de saisie de caratère qui contrôle la saisie.
+// param       Reçoit les valeur de MIN_CHAR et MAX_CHAR
+// return      Un char
+// exception   n/a
+//***********************************************************************************
+char getC(const char MIN_CHAR, const char MAX_CHAR);
 
-}
 
-char getChar() {
-
-}
-
-bool tryAgain() {
-
-}
+//***********************************************************************************
+// nom         tryAgain
+// but         Fonction qui permet de retourner un booléen.
+// param       Aucun
+// return      Un booléen
+// exception   n/a
+//***********************************************************************************
+bool tryAgain();
 
 #endif //LABO05_CHRONO_FONCTION_H
